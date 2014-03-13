f026721659236ed11e98d164ba38b52536a0d0b6
e-spin <git@e-spin.de>
2014-03-13 17:51:40 +0100

    comment

--- start_compass-scss.bat
+++ start_compass-scss.bat
@@ -1,3 +1,12 @@
+:: compass starter
+::
+:: Copyright (C) 2014, e-spin Berlin 
+::
+:: @package compass starter
+:: @author  e-spin Berlin <http://www.e-spin.de>
+:: @author  Ingolf Steinhardt <info@e-spin.de> 
+:: @license Commercial
+
 @ECHO OFF
 SET SCRIPT_LOCATION=%~dp0
 SET SxSS_LOCATION=scss
